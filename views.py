c617b131b3b87364eae085dc27cec0d3c041369e
kamanbo23 <79871646+kamanbo23@users.noreply.github.com>
2024-08-25 22:45:17 -0700

    Update views.py

--- views.py
+++ views.py
@@ -1,1 +1,17 @@
+from rest_framework import viewsets
+from .models import Paper, Feedback
+from .serializers import PaperSerializer, FeedbackSerializer, UserSerializer
+from django.contrib.auth.models import User
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PaperViewSet(viewsets.ModelViewSet):
+    queryset = Paper.objects.all()
+    serializer_class = PaperSerializer
+
+class FeedbackViewSet(viewsets.ModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
 
